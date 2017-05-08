1a8e66f04c8b7465180fce809fc6620b23e00eaf
daattavya98 <daattavya.aggarwal98@gmail.com>
2017-05-09 00:09:37 +0530

    Add files via upload

---
+++ Nano.ino
@@ -0,0 +1,130 @@
+#include<SoftwareSerial.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+MPU6050 accelgyro;
+SoftwareSerial bt(2, 3);
+
+int btArduinoBaud = 9600;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+#define OUTPUT_READABLE_ACCELGYRO
+
+// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
+// binary, one right after the other. This is very fast (as fast as possible
+// without compression or data loss), and easy to parse, but impossible to read
+// for a human.
+//#define OUTPUT_BINARY_ACCELGYRO
+
+
+#define LED_PIN 13
+bool blinkState = false;
+long long int x,y;
+void setup() {
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  Serial.begin(38400);
+  Serial.println("Initializing I2C devices...");
+  accelgyro.initialize();
+
+  Serial.println("Testing device connections...");
+  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+  bt.begin(btArduinoBaud);
+
+
+  // use the code below to change accel/gyro offset values
+  /*
+    Serial.println("Updating internal sensor offsets...");
+    // -76  -2359 1688  0 0 0
+    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+    Serial.print("\n");
+    accelgyro.setXGyroOffset(220);
+    accelgyro.setYGyroOffset(76);
+    accelgyro.setZGyroOffset(-85);
+    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+    Serial.print("\n");
+  */
+
+  // configure Arduino LED for
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+
+void loop() {
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+#ifdef OUTPUT_READABLE_ACCELGYRO
+  // display tab-separated accel/gyro x/y/z values
+  Serial.print("a/g:\t");
+  Serial.print(ax); Serial.print("\t");
+  Serial.print(ay); Serial.print("\t");
+  Serial.print(az); Serial.print("\t");
+  Serial.print(gx); Serial.print("\t");
+  Serial.print(gy); Serial.print("\t");
+  Serial.println(gz);
+#endif
+
+#ifdef OUTPUT_BINARY_ACCELGYRO
+  Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
+  Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
+  Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
+  Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
+  Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
+  Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+#endif
+//bt.write('r');
+
+
+x=map(ax,-8000,8000,0,255);
+y=map(ay,-8000,8000,0,255);
+
+
+char t;
+
+
+
+
+while(!bt.available());
+t = bt.read();
+Serial.println(t);
+
+if(t=='x')
+{
+  delay(50);
+  bt.print(char(x));
+}
+else if(t=='y')
+{
+  delay(50);
+  bt.print(char(y));
+}
+
+
+
+
+ 
+  // blink LED to indicate activity
+  blinkState = !blinkState;
+  digitalWrite(LED_PIN, blinkState);
+}
---
+++ Uno.ino
@@ -0,0 +1,181 @@
+//Final code for uno.
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTserial(2, 3); // RX | TX
+// Connect the HC-05 TX to Arduino pin 2 RX. 
+// Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
+// Working 
+ 
+char c1=' ',c2=' ';//below lines define the pins for the motor and the electromagnetic relay.
+int x=0,y=0;
+int pwm1=11; 
+int in1=10;
+int in2=9;
+int in3=8;
+int in4=7;
+int pwm2=6;
+int relay_in1= 5;
+int relay_gnd= 12;
+int relay_vcc= 13;
+void setup() 
+{
+    Serial.begin(38400);
+    Serial.println("Arduino is ready");
+    Serial.println("Remember to select Both NL & CR in the serial monitor");
+ 
+    // HC-05 default serial speed for AT mode is 38400
+    BTserial.begin(9600);
+  pinMode(pwm1,OUTPUT);
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+  pinMode(pwm2,OUTPUT);
+  pinMode(relay_in1,OUTPUT);
+  pinMode(relay_gnd,OUTPUT);
+  pinMode(relay_vcc,OUTPUT);
+}
+ 
+void loop()
+{
+  digitalWrite(relay_gnd,LOW);
+  digitalWrite(relay_vcc,HIGH);
+  digitalWrite(relay_in1,HIGH);
+     BTserial.write('x');//When the bluetooth writes x then the master bluetooth will send x reading of accelerometer as that is how we have written the logic of the program.
+     
+     while(!BTserial.available());
+
+        c1 = BTserial.read();
+        x=(int)c1;
+        
+        if(x<0)
+        {
+          x=map(x,-128,-1,-1,-128);//the readings of the accelerometer are high when pwm to be given is low and vice versa. Hence we have mapped the readings in this way.
+        }
+        else
+        {
+          x=map(x,0,127,127,0);
+        }
+
+
+        
+        Serial.print("  x: ");
+        Serial.print(x);
+        BTserial.flush();
+       Serial.flush();
+   
+     BTserial.write('y');
+      while(!BTserial.available());
+        c2 = BTserial.read();
+        y=(int)c2;
+        if(y<0)
+        {
+         y=map(y,-128,-1,-1,-128);
+        }
+        else
+        {
+         y=map(y,0,127,127,0);
+        }
+        Serial.print("  y: ");
+         Serial.print(y);
+         Serial.println(" ");
+        BTserial.flush();
+        Serial.flush();
+     
+
+    
+
+
+//Below we have the logic for driving the car in the required direction based on the values of x and y obtained( which are the accelerations)
+//in1,in2,in3 and in4 are the pins for the polarity of the motor and pwm1,pwm2 allow us to provide the required pwm.
+    if(y>15)
+    {
+        if(x<-30)
+       {
+
+
+            
+            digitalWrite(in1,HIGH);
+            digitalWrite(in2,HIGH);
+            digitalWrite(in3,HIGH);
+            digitalWrite(in4,LOW);
+            
+       }
+       else if(x>30)
+       {
+            digitalWrite(in1,LOW);
+            digitalWrite(in2,HIGH);
+            digitalWrite(in3,HIGH);
+            digitalWrite(in4,HIGH);
+
+            
+       }
+       else
+    {
+
+           digitalWrite(in1,LOW);
+           digitalWrite(in2,HIGH);
+           digitalWrite(in3,HIGH);
+           digitalWrite(in4,LOW);
+           
+     }
+
+          analogWrite(pwm1,y);
+          analogWrite(pwm2,y);
+    } 
+
+
+
+
+
+    
+    else if(y<-15)
+    { 
+        if(x<-30)
+        {
+ 
+
+       
+               digitalWrite(in1,HIGH);
+             digitalWrite(in2,HIGH);
+             digitalWrite(in3,LOW);
+             digitalWrite(in4,HIGH);  
+        }
+        else if(x>30)
+        {
+
+                digitalWrite(in1,HIGH);
+             digitalWrite(in2,LOW);
+             digitalWrite(in3,HIGH);
+             digitalWrite(in4,HIGH);
+           
+        }
+        else
+        {
+
+            digitalWrite(in1,HIGH);
+        digitalWrite(in2,LOW);
+        digitalWrite(in3,LOW);
+        digitalWrite(in4,HIGH);
+      }
+
+        y=(-y);
+
+           
+        analogWrite(pwm1,y);
+        analogWrite(pwm2,19.0*y/20.0);
+    }
+
+
+
+    else 
+    {
+
+         digitalWrite(in1,HIGH);
+         digitalWrite(in2,HIGH);
+         digitalWrite(in3,HIGH);
+         digitalWrite(in4,HIGH);
+    }
+
+  
+}
